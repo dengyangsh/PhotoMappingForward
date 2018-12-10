13d1f70b92c37be4f20d9de0f242fb51d019e55f
dengbin <dengbin@hebao.im>
2018-12-10 18:35:30 +0800

    mod 提取公共变量

--- js/ProjectVariable.js
+++ js/ProjectVariable.js
@@ -1,22 +1,22 @@
-// var projectVariable= {
-//     getPhotoUrl: "http://localhost:9002/photo/provincePhoto",
-//     loginUrl:"http://localhost:9002/user/login",
-//     registUrl: "http://localhost:9002/user/register",
-//     uploadUrl:"http://localhost:9002/photo/uploadPhoto",
-//     loginHtml:"./login.html",
-//     myMapHtml : "./myMap.html",
-//     uploadHtml:"./upload.html",
-//     specificMapHtml : "./specificMap.html"
-// };
-
-
 var projectVariable= {
-    getPhotoUrl: "http://129.204.14.220:82/photo/provincePhoto",
-    loginUrl:"http://129.204.14.220:82/user/login",
-    registUrl: "http://129.204.14.220:82/user/register",
-    uploadUrl:"http://129.204.14.220:82/photo/uploadPhoto",
+    getPhotoUrl: "http://localhost:9002/photo/provincePhoto",
+    loginUrl:"http://localhost:9002/user/login",
+    registUrl: "http://localhost:9002/user/register",
+    uploadUrl:"http://localhost:9002/photo/uploadPhoto",
     loginHtml:"./login.html",
     myMapHtml : "./myMap.html",
     uploadHtml:"./upload.html",
     specificMapHtml : "./specificMap.html"
 };
+
+
+// var projectVariable= {
+//     getPhotoUrl: "http://129.204.14.220:82/photo/provincePhoto",
+//     loginUrl:"http://129.204.14.220:82/user/login",
+//     registUrl: "http://129.204.14.220:82/user/register",
+//     uploadUrl:"http://129.204.14.220:82/photo/uploadPhoto",
+//     loginHtml:"./login.html",
+//     myMapHtml : "./myMap.html",
+//     uploadHtml:"./upload.html",
+//     specificMapHtml : "./specificMap.html"
+// };
